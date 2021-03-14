7e09cbb03fccc476922e2c068dc15150d85b6f51
MazhenkouA <@Mozhenkov>
2021-03-14 20:22:16 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let hash = {}, stack = [], curr;
+  for (var i = 0; i < bracketsConfig.length; ++i)
+    hash[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  for (let i = 0; i < str.length; i += 1) {
+    curr = str[i];
+    if (hash[curr]) stack.push(curr);
+    else {
+      if (curr !== hash[stack.pop()]) return false;
+    }
+  }
+  return stack.length === 0;
 }
